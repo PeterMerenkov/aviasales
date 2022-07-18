a2b3957110f1c9c35aff8c92315ae074eea6c1bd
Peter Merenkov <peter.merenkov@gmail.com>
2022-07-18 14:31:49 +0400

    delete garbage from booking

--- booking/src/main/java/ru/opencode/practice/BookingApp.java
+++ booking/src/main/java/ru/opencode/practice/BookingApp.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookingApp
 {
-    //test commit
     public static void main( String[] args )
     {
         SpringApplication.run(BookingApp.class, args);
