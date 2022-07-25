a9c69acfc8a1c306a7c0df07dfdecd5565299332
Grigorii <osagrigoriy@mail.ru>
2022-07-25 20:06:43 +0400

    Test thymeleaf

--- timetable/src/main/java/ru/opencode/practice/timetable/controllers/GreetingContoller.java
+++ timetable/src/main/java/ru/opencode/practice/timetable/controllers/GreetingContoller.java
@@ -1,11 +1,10 @@
 package ru.opencode.practice.timetable.controllers;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.opencode.practice.timetable.model.helpers.HumanNeeds;
 import ru.opencode.practice.timetable.service.AdmineService;
 
@@ -14,15 +13,26 @@ import ru.opencode.practice.timetable.service.AdmineService;
 public class GreetingContoller {
     @Autowired
     AdmineService admineService;
-    @RequestMapping("search")
-    public String search(){
-        return "search.html";
+
+    @RequestMapping("searchGet")
+    public String searchGet() {
+        return  "search";
     }
 
     @PostMapping
     @RequestMapping("/getFlights")
-    public String takeFlights(@RequestBody HumanNeeds info, Model model) {
+    public String takeFlights(@RequestBody HumanNeeds info, Model model,@PathVariable Test test) {
         model.addAttribute("personTicketView", admineService.takeFlights(info.arrival_city(), info.departure_city(), info.fare_conditions(), info.amount(), info.skip()));
-        return "search.html";
+        model.addAttribute("amount",info.amount());
+        model.addAttribute("1",1);
+        model.addAttribute("test",test);
+        System.out.printf("TESTINGGGGGGGGGGGGGGGGGGGGGGG\nTESTINGGGGGGGGGGGGGGGGG");
+        return  "search";
     }
+
+}
+@Data
+class Test{
+    private String test;
+
 }
